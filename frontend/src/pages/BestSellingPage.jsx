3894aec1a4197398f770c27f5203173747c3b794
battlesofarmy <battlesofarmy@gmail.com>
2025-04-14 10:10:08 +0600

    test

--- frontend/src/pages/BestSellingPage.jsx
+++ frontend/src/pages/BestSellingPage.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Header from "../components/Layout/Header";
 import Loader from "../components/Layout/Loader";
