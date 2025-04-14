bbf6b1cf967c453c4f6e138d6fd67b48bd22978b
battlesofarmy <battlesofarmy@gmail.com>
2025-04-14 10:19:18 +0600

    check

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 // }));
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000', // Frontend URL
+  origin:[ 'http://localhost:3000', 'https://multivendor-frontend-lyart.vercel.app'], // Frontend URL
   credentials: true               // Allow cookies, auth headers, etc.
 }));
 
