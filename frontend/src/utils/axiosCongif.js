8e67bb044501298d7b0b5eeadf265b15721bc982
battlesofarmy <battlesofarmy@gmail.com>
2025-04-19 14:40:08 +0600

    start from add to cart/wishlist code

--- frontend/src/utils/axiosCongif.js
+++ frontend/src/utils/axiosCongif.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
-    // baseURL: "https://multivendor-backend-henna.vercel.app/api/v2",
+    // baseURL: "http://localhost:5000",
+    baseURL: "https://multivendor-backend-henna.vercel.app/api/v2",
     timeout: 1000,
     headers: {"Content-Type": "application/json"}
 });
